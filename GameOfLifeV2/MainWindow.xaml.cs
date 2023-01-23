706cad18530fd4374416f13e2fd2d6bf3335222d
Adrian Lion <adrian.lion@online.de>
2023-01-23 17:49:50 +0100

    Implementiert Start/Stop funktion

--- GameOfLifeV2/MainWindow.xaml.cs
+++ GameOfLifeV2/MainWindow.xaml.cs
@@ -21,23 +21,39 @@ namespace GameOfLifeV2
     public partial class MainWindow : Window
     {
         private Board? _board;
-        
+
         public MainWindow()
         {
             InitializeComponent();
+            // UpdateAllCellsOnClick abboniert(subscribed) den EventPublisher MouseDown(Click) vom PlotArea
+            PlotArea.MouseDown += UpdateAllCellsOnClick;
+        }
+
+        private void UpdateAllCellsOnClick(object sender, MouseButtonEventArgs e)
+        {
+            // ? -> führe alles hinter dem . also nach ?. nur aus, wenn _board nicht NULL ist
+            _board?.CountNeighboursNoBorders();
+            // Handled = true -> das click event wurde behandelt
+            e.Handled = true;
         }
 
         private void BtnInit_OnClick(object sender, RoutedEventArgs e)
         {
             _board = new Board(PlotArea);
-            _board.InitBoard(PlotArea);
-            _board.SearchNeighbours();
-              
+            // ! -> Compiler meckert, dass _board NULL sein könnte: !. "Hey Compiler! Das ist nicht NULL, glaub mir!"
+            _board!.InitBoard(PlotArea);
+
+            // Braucht man beim Init noch nicht.
+            //_board!.SearchNeighbours();
         }
 
         private void BtnStartStop_OnClick(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            // gleiches problem, wie in UpdateAllCellsOnClick
+            _board?.CountNeighboursNoBorders();
+            _board?.SetNewStatus();
+            _board?.CountNeighboursNoBorders();
+
         }
     }
 }
